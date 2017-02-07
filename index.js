e8b7a62d2e901edaf718b9c697171b6c75c6d7a3
nitaybz <nitaybz@gmail.com>
2017-02-07 20:09:35 +0200

    fix

--- index.js
+++ index.js
@@ -1,35 +1,36 @@
-"use strict";
-
-var Service, Characteristic;
+var Service, Characteristic, Accessory;
 
 module.exports = function(homebridge) {
  
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
-  
+  Accessory = homebridge.hap.Accessory;
   homebridge.registerAccessory("homebridge-delay-switch", "DelaySwitch", DelaySwitch);
 }
 
 function DelaySwitch(log, config) {
   this.log = log;
-  this.name = config.name;
-  this.delayTime = config.delay;
+  this.name = config['name'];
+  this.delayTime = config['delay'];
   this.Timer;
-  this._service = new Service.Switch(this.name);
-  this._service.getCharacteristic(Characteristic.On)
-    .on('set', this._setOn.bind(this));
 }
 
 DelaySwitch.prototype.getServices = function() {
+ this.service = new Service.Switch(this.name);
+ 
+ this.service.addCharacteristic(Characteristic.On);
+    this.service.getCharacteristic(Characteristic.On)
+        .on('set', this.setOn.bind(this));
+ 
   return [this._service];
 }
 
-DelaySwitch.prototype._setOn = function(on, callback) {
+DelaySwitch.prototype.setOn = function(on, callback) {
  this.log("Setting switch to " + on);
  if (on) {
     this.Timer = setTimeout(function() {
-      this._service.setCharacteristic(Characteristic.On, false);
-    }.bind(this), this.delayTime);
+      this.service.setCharacteristic(Characteristic.On, false);
+    }, this.delayTime);
   }
   else { 
    clearTimeout(this.Timer);
